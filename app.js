e45c685ca8fbaf82f41f0bd9c47b2283c44c6d25
Mohammad Sikandar <mohammadsikandar464@gmail.com>
2019-03-28 10:28:59 +0530

    initials

---
+++ app.js
@@ -0,0 +1,65 @@
+let userScore=0;
+let computerScore=0;
+const userScore_span=document.getElementById("user-score");
+const computerScore_span=document.getElementById("computer-score");
+const scoreBoard_div=document.querySelector("score-board");
+const result_div=document.querySelector(".result");
+const rock_div=document.getElementById("r");
+const paper_div=document.getElementById("p");
+const scissors_div=document.getElementById("s");
+
+function getComputerChoice(){
+ const choices =['r','p','s'];
+ const randomNumber=Math.floor(Math.random()*3);
+ return choices[randomNumber];
+}
+
+function win(userChoice,computerChoice){
+	userScore++;
+	userScore_span.innerHTML = userScore;
+    console.log(userChoice);
+    console.log(computerChoice);
+}
+function lose(){
+	console.log("user loose");
+}
+function draw(){
+	console.log("user draw");
+}
+
+function game(userChoice){
+  const computerChoice=getComputerChoice();
+  switch(userChoice+computerChoice){
+  	case "rs":
+  	case "pr":
+  	case "sp":
+  	win(userChoice,computerChoice);
+  	break;
+  	case "rp":
+  	case "ps":
+  	case "sr":
+  	lose();
+  	break;
+  	case "rr":
+  	case "pp":
+  	case "ss":
+    draw();
+    break;
+  }
+}
+
+
+function main(){
+	rock_div.addEventListener('click',function(){
+	game("r");
+})
+
+paper_div.addEventListener('click',function(){
+	game("p");
+})
+
+scissors_div.addEventListener('click',function(){
+	game("s");
+})
+}
+main();
